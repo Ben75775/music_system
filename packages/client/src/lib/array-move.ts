401578c57804adb3095170d7d515053efe1bfd43
Ben75775 <elay96@gmail.com>
2026-04-24 13:01:03 +0300

    feat(lib): add arrayMove helper with tests

---
+++ packages/client/src/lib/array-move.ts
@@ -0,0 +1,6 @@
+export function arrayMove<T>(arr: readonly T[], from: number, to: number): T[] {
+  const next = arr.slice();
+  const [item] = next.splice(from, 1);
+  next.splice(to, 0, item);
+  return next;
+}
